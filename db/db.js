fe36267ce6353a6d8004b4f1ffbbcfe19b2590b2
jode401 <jeoper21@gmail.com>
2026-04-10 13:27:56 +0100

    hey

---
+++ db/db.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose')
+ require('dotenv').config()
+mongoose.connect(process.env.MongoUri)
+.then(() => {
+    console.log("Connected successfully!");
+})
+.catch((e) => {
+    console.log("Connection error:", e);
+});
+
+const form=mongoose.Schema({
+    email:{
+type:String,
+required:true
+    },
+    message:{
+type:String,
+required:true
+    },
+    budget:{
+type:String,
+required:true
+    },service:{
+type:String,
+required:true
+    },company:{
+type:String,
+required:true
+    },name:{
+type:String,
+required:true
+    }
+})
+const model=mongoose.model("PROTENV",form)
+
+module.exports={
+    model
+}
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const model=require('./db/db')
+ require('dotenv').config()
+ var sanitize = require('mongo-sanitize');
 const path = require('path');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +15,18 @@ app.get('/', (req, res) => res.render('index'));
 app.get('/services', (req, res) => res.render('services'));
 app.get('/work', (req, res) => res.render('work'));
 app.get('/contact', (req, res) => res.render('contact'));
+app.post('/form', async (req, res) => {
+    try {
+        const cleanInput = sanitize(req.body);
+        const { message, budget, service, company, name, email } = cleanInput
+    
+        await model.model.create({ message, budget, service, company, name, email });
 
+        res.redirect('/');
+    } catch (e) {
+        console.error("Error saving to database:", e);
+        res.status(500).send("An error occurred");
+    }
+});
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));     
