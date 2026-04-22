4b0c14d9817075a145aaaf9868d2166b80fb6fed
jode401 <jeoper21@gmail.com>
2026-04-22 21:56:43 +0100

    hey

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
  require('dotenv').config()
-mongoose.connect(process.env.MongoUri)
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("Connected successfully!");
 })
